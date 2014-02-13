5fc1de18ed6c2a6abc7de7d5a06e0a3b57298d50
Leo Picado <leopic@gmail.com>
2014-02-12 18:46:58 -0600

    Cambiando usuario de franco.

--- js/controllers.js
+++ js/controllers.js
@@ -2,12 +2,11 @@ angular.module('testApp.controllers', [
     'testApp.services'
 ])
 .controller('FeedCtrl', function ($scope, githubService, sayHelloService) {
-    $scope.users = ['KaeruCT', 'leopic', 'pfallasro', 'haden9', 'frivera1984'];
+    $scope.users = ['KaeruCT', 'leopic', 'pfallasro', 'haden9', 'mfranco84'];
 
     $scope.viewUserEvents = function (user) {
 
         $scope.greeting = sayHelloService.sayHello(user);
-
         githubService.setUser(user);
         githubService.events()
         .success(function(data, status, headers) {
